96a3e84346033e47e034618ce6d5900f7f9ae510
Mayke Fernandes dos Santos <Mayke Fernandes dos Santos>
2016-09-17 22:45:05 -0300

    alterações feitas em controller e view

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,7 +1,7 @@
 angular.module('app.controllers', [])
 
-    .service('sharedProperties', function() {
-    var objectValue = { nome: ''};
+.service('escolaPropriedades', function() {
+    var objectValue = {};
     
     return {
         setObject: function(value){
@@ -13,50 +13,68 @@ angular.module('app.controllers', [])
     }
 })
   
-.controller('educaNota10Ctrl', ['$scope', '$stateParams', '$http', 'sharedProperties',
-  function ($scope, $stateParams, $http, sharedProperties) {
-    pg = 0;  
-    sharedProperties.setObject(nome= 'casass1212');
-    $scope.testeAlteracao = sharedProperties.getObject();
+.controller('educaNota10Ctrl', ['$scope', '$stateParams', '$http', 'escolaPropriedades',
+function ($scope, $stateParams, $http, escolaPropriedades) 
+{
+    pg = 0;
+
+    $scope.cliked = (function(escola)
+    {       
+        escolaPropriedades.setObject(escola);
+    });
+
+// // // codigo teste para GeoLocalização 
+    // if (navigator.geolocation) 
+    // {
+    //     navigator.geolocation.getCurrentPosition(function(position)
+    //     {
+    //         $scope.$apply(function()
+    //         {
+    //             $scope.position = position;
+    //             console.log($scope.position);
+    //         });
+    //     });
+    // }
+
     $scope.escolas = [];
     $escolasArray = [];
-    $scope.loadMore = function() {
-        url = $http.get("http://mobile-aceite.tcu.gov.br:80/nossaEscolaRS/rest/escolas?pagina=" + pg + "&quantidadeDeItens=15").success(function(listaEscola) {
-        $escolasArray = $escolasArray.concat(listaEscola);
-        $scope.escolas = $escolasArray;
-        console.log($escolasArray);
-        $scope.$broadcast('scroll.infiniteScrollComplete');
-        pg++;
-    });    
-  };
+
+    $scope.loadMore = function() 
+    {
+        url = $http.get("http://mobile-aceite.tcu.gov.br:80/nossaEscolaRS/rest/escolas?pagina=" + pg + "&quantidadeDeItens=15").success(function(listaEscola) 
+        {
+            $escolasArray = $escolasArray.concat(listaEscola);
+            $scope.escolas = $escolasArray;
+            console.log($escolasArray);
+            $scope.$broadcast('scroll.infiniteScrollComplete');
+            pg++;
+        });    
+    };
 }])
    
-.controller('estatSticasCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
-// You can include any angular dependencies as parameters for this function
-// TIP: Access Route Parameters for your page via $stateParams.parameterName
+.controller('estatSticasCtrl', ['$scope', '$stateParams', 
 function ($scope, $stateParams) {
 
 
 
 }])
    
-.controller('histRicoCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
-// You can include any angular dependencies as parameters for this function
-// TIP: Access Route Parameters for your page via $stateParams.parameterName
+.controller('histRicoCtrl', ['$scope', '$stateParams', 
 function ($scope, $stateParams) {
 
 }])
-.controller('informaEsCtrl', ['$scope', '$stateParams', 'sharedProperties',
-function ($scope, $stateParams, sharedProperties) {
-  $scope.escolaObj = sharedProperties.getObject();
-
+         
+.controller('informaEsCtrl', ['$scope', '$stateParams', 'escolaPropriedades',
+function ($scope, $stateParams, escolaPropriedades) 
+{
+    $scope.$on('$ionicView.beforeEnter', function () 
+    {
+        $scope.escola = escolaPropriedades.getObject();
+    });
 }])
    
-.controller('resultadoDaBuscaCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
-// You can include any angular dependencies as parameters for this function
-// TIP: Access Route Parameters for your page via $stateParams.parameterName
+.controller('resultadoDaBuscaCtrl', ['$scope', '$stateParams',
 function ($scope, $stateParams) {
 
 
 }])
- 
